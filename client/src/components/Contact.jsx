e8a50aa9d07e7904829b03417b8630966a63a2fb
Chijioke Chika <8pluscreative@gmail.com>
2023-07-14 05:18:32 +0100

    Chats

--- client/src/components/Contact.jsx
+++ client/src/components/Contact.jsx
@@ -7,7 +7,52 @@ const Contact = ({ contacts, currentUser }) => {
   const [currentUserImage, setCurrentUserImage] = useState("");
   const [currentSelected, setCurrentSelected] = useState("");
 
-  return <div>Contact</div>;
+  useEffect(() => {
+    if (currentUser) {
+      setCurrentUsername(currentUser.username);
+      setCurrentUserImage(currentUser.avatarImage);
+    }
+  }, [currentUser]);
+
+  const changeCurrentChat = (index, contact) => {};
+
+  return (
+    <>
+      {currentUserImage && currentUsername && (
+        <Container>
+          <div className="brand">
+            <img src={Logo} alt="logo" />
+            <h3>Snappy</h3>
+          </div>
+
+          <div className="contacts">
+            {contacts.map((contact, index) => {
+              return (
+                <div
+                  key={index}
+                  className={`contact ${
+                    index === currentSelected ? "selected" : ""
+                  }`}
+                >
+                  <div className="avatar">
+                    <img
+                      src={`data:image/svg+xml;base64,${contact.avatarImage}`}
+                      alt="avatar"
+                    />
+                  </div>
+                  <div className="username">
+                    <h3>{contact.username}</h3>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        </Container>
+      )}
+    </>
+  );
 };
+
+const Container = styled.div``;
 
 export default Contact;
