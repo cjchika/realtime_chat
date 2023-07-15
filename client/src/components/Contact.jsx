eeb71dd3389314a5e4d17d46442cf45890d3cfd6
Chijioke Chika <8pluscreative@gmail.com>
2023-07-15 07:19:18 +0100

    Chat

--- client/src/components/Contact.jsx
+++ client/src/components/Contact.jsx
@@ -47,12 +47,99 @@ const Contact = ({ contacts, currentUser }) => {
               );
             })}
           </div>
+          <div className="current-user">
+            <div className="avatar">
+              <img
+                src={`data:image/svg+xml;base64,${currentUserImage}`}
+                alt="avatar"
+              />
+            </div>
+          </div>
+          <div className="username">
+            <h2>{currentUsername}</h2>
+          </div>
         </Container>
       )}
     </>
   );
 };
 
-const Container = styled.div``;
+const Container = styled.div`
+  display: grid;
+  grid-template-rows: 10% 75% 15%;
+  overflow: hidden;
+  background-color: #080420;
+  .brand {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 1rem;
+    img {
+      height: 2rem;
+    }
+    h3 {
+      color: white;
+      text-transform: uppercase;
+    }
+  }
+  .contacts {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    overflow: auto;
+    gap: 0.8rem;
+    .contact {
+      background-color: #ffffff39;
+      min-height: 5rem;
+      width: 90%;
+      cursor: pointer;
+      border-radius: 0.2rem;
+      padding: 0.4rem;
+      gap: 1rem;
+      align-items: center;
+      display: flex;
+      transition: 0ms.5s ease-in-out;
+      .avatar {
+        img {
+          height: 3rem;
+        }
+      }
+      .username {
+        h3 {
+          color: white;
+        }
+      }
+    }
+    .selected {
+      background-color: #9186f3;
+    }
+  }
+  .current-user {
+    background-color: #0d0d30;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 2rem;
+    .avatar {
+      img {
+        height: 4rem;
+        max-inline-size: 100%;
+      }
+    }
+    .username {
+      h2 {
+        color: white;
+      }
+    }
+    @media screen and (min-width: 720px) and (max-width: 1200px) {
+      gap: 0.5rem;
+      .username {
+        h2 {
+          font-size: 1rem;
+        }
+      }
+    }
+  }
+`;
 
 export default Contact;
