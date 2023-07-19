8db528cecf5b87772315bf5c1671219c774b43cf
Chijioke Chika <8pluscreative@gmail.com>
2023-07-19 05:11:24 +0100

    Chat

--- server/controllers/messagesController.js
+++ server/controllers/messagesController.js
@@ -1,8 +1,19 @@
 const Messages = require("../models/messageModel.js");
-const bcrypt = require("bcrypt");
 
 module.exports.addMessage = async (req, res, next) => {
   try {
+    const { from, to, message } = req.body;
+    const data = await Messages.create({
+      message: { text: message },
+      users: [from, to],
+      sender: from,
+    });
+
+    if (data) {
+      return res.json({ msg: "Messages added successfully." });
+    }
+
+    return res.json({ msg: "Failed to add message to the database." });
   } catch (error) {
     next(error);
   }
